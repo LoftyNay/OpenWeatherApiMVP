5da660627f7152fed635e9835ee7f9ffda3f6b69
Artem <wiztema@gmail.com>
2019-04-18 18:52:08 +0300

    test project

--- app/src/main/kotlin/com/ltn/openweather/ui/activities/main/view/MainActivityView.kt
+++ app/src/main/kotlin/com/ltn/openweather/ui/activities/main/view/MainActivityView.kt
@@ -1,9 +1,12 @@
 package com.ltn.openweather.ui.activities.main.view
 
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.ltn.openweather.base.BaseView
 import com.ltn.openweather.model.Weather
 
-interface MainActivityView: BaseView {
+@StateStrategyType(value = OneExecutionStateStrategy::class)
+interface MainActivityView : BaseView {
     fun showWeathersList(weathersList: MutableList<Weather>)
     fun showToast(message: String)
 }
